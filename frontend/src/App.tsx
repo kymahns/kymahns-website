1ccd717b6a73b407cbb0c181c3677e9c56f49952
kymahns <hoangnamvn126@gmail.com>
2026-03-20 16:23:04 +0000

    feat: removed all bloat and temporary code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,9 @@
 import './App.css'
-import logo from "/public/logos/logo.svg"
 
 function App() {
   return (
     <>
-      <section id="center">
-        <div className="hero" style={{ height: "100vh" }}>
-          <img src = {logo} alt="logo" />
-          <h1>Nam's Website Portfolio</h1>
-        </div>
-      </section>
+      
     </>
   )
 }
